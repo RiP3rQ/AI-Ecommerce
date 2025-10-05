b8e81700a916977b643582cc19cc4050d9e28a52
Rafał Pompa <rafalpompa2000@gmail.com>
2025-10-05 17:50:29 +0200

    feat: add marquee component with server-side fetching for latest products and integrate Tailwind CSS container queries plugin

---
+++ src/components/marquee/actions.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import { drizzleDbClient } from "@/database";
+import { createServerSupabaseClient } from "@/supabase-auth/server";
+import {
+  productImages,
+  products,
+  productVariants,
+  SelectProduct,
+  SelectProductImage,
+  SelectProductVariant,
+} from "@/database/schema";
+import { and, desc, eq } from "drizzle-orm";
+import { getErrorMessage } from "@/lib/utils";
+
+export type Latest20ProductsReturnType = {
+  products: SelectProduct;
+  product_images: SelectProductImage | null;
+  product_variants: SelectProductVariant | null;
+};
+
+export async function getLatest20Products(): Promise<
+  Latest20ProductsReturnType[]
+> {
+  try {
+    // Step 0: Validate session
+    const supabaseServer = await createServerSupabaseClient();
+    const {
+      data: { user },
+    } = await supabaseServer.auth.getUser();
+
+    if (!user) {
+      throw new Error("User is not authenticated");
+    }
+
+    // Step 1: Get latest 20 products with main image (order = 1)
+    const latest20Products = await drizzleDbClient()
+      .select()
+      .from(products)
+      .leftJoin(
+        productImages,
+        and(
+          eq(products.id, productImages.productId),
+          eq(productImages.order, 1)
+        )
+      )
+      .leftJoin(productVariants, eq(products.id, productVariants.productId))
+      .orderBy(desc(products.createdAt))
+      .limit(20);
+
+    if (!latest20Products) {
+      throw new Error("Latest 20 products not found");
+    }
+
+    return latest20Products;
+  } catch (error: unknown) {
+    console.error(`[ERROR] Failed to get latest 20 products:`, error);
+    throw new Error(getErrorMessage(error));
+  }
+}
