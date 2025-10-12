af60a8f11d4f8b0ee635e4ca449f7feaf5b3177a
Rafał Pompa <rafalpompa2000@gmail.com>
2025-10-12 15:51:36 +0200

    feat: enhance drizzleDbClient to include schema and set casing to snake_case for improved database interaction

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,15 @@
 import { env } from "../env";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import * as schema from "./schema";
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(env.DATABASE_URL, { prepare: false });
 
 export function drizzleDbClient() {
-  return drizzle({ client });
+  return drizzle({
+    client,
+    schema,
+    casing: "snake_case",
+  });
 }
